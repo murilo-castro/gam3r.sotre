73a0b6239a119dd5254edfa8216f6835a2d4489c
Murilo da Silva Castro <murilo.65@gmail.com>
2024-10-01 23:26:35 -0300

    fix: repair the bug imports

---
+++ .history/src/app/page_20241001232621.tsx
@@ -0,0 +1,14 @@
+import ProductItem from "@/components/product/ProductItem";
+import { products } from "@/core";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <div className="grid grid-cols-4">
+        {products.map((product) => (
+          <ProductItem key={product.id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
+}
