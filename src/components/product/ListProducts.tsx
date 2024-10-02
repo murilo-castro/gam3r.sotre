4c6d27bd8fdce225339188d339b7afb410b43897
Murilo da Silva Castro <murilo.65@gmail.com>
2024-10-02 18:43:18 -0300

    fix: Fixed bug when importing product not found

--- src/components/product/ListProducts.tsx
+++ src/components/product/ListProducts.tsx
@@ -6,7 +6,7 @@ import ProductNotFound from "./ProductNotFound";
 
 export default function ListProducts() {
   // const { produtos } = useProdutos()
-  return !products.length ? (
+  return products.length ? (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 ">
       {products.map((product) => (
         <ProductItem product={product} key={product.id} />
